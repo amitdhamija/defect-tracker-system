5c7347e05950625eade375ce125b38ad7e322075
= <tcribbet@gmail.com>
2014-09-07 17:48:41 -0400

    updated to integrate with database helper

--- x46011/teama/dts/model/Person.java
+++ x46011/teama/dts/model/Person.java
@@ -1,23 +1,41 @@
 package x46011.teama.dts.model;
 
 public class Person {
-	private String Name;
+	private int ID;
+	private String FirstName;
+	private String LastName;
 	private String Email;
-	public Person(String name, String email)
+	public Person(int Id, String firstName, String lastName, String email)
 	{
-		setName(name);
+		setId(Id);
+		setName(firstName, lastName);
 		setEmail(email);
 	}
-	public String getName() {
-		return Name;
+	public Person(String firstName, String lastName, String email)
+	{
+		setName(firstName, lastName);
+		setEmail(email);
 	}
-	public void setName(String name) {
-		Name = name;
+	public String getFirstName() {
+		return FirstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setName(String firstName, String lastName) {
+		FirstName = firstName;
+		LastName = lastName;
 	}
 	public String getEmail() {
 		return Email;
 	}
 	public void setEmail(String email) {
 		Email = email;
+	}
+	public int getId() {
+		return ID;
+	}
+	public void setId(int Id) {
+		ID = Id;
 	}
 }
