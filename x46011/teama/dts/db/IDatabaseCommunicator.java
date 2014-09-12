4b385f954ea6c0cef5069d9a56ef2f029de2a091
amitdhamija <khiladi.ad@gmail.com>
2014-09-11 17:44:42 -0700

    Renamed the interface to match the implemented class name

---
+++ x46011/teama/dts/db/IDatabaseCommunicator.java
@@ -0,0 +1,35 @@
+package x46011.teama.dts.db;
+
+import java.util.List;
+
+import x46011.teama.dts.model.Defect;
+import x46011.teama.dts.model.Person;
+import x46011.teama.dts.model.Status;
+
+
+/**
+ * IDTSCommManager provides an interface for DTSCommManager to enable modularity.
+ * 
+ * @author Travis Cribbet
+ * @version 1.0
+ * @revision 1.1	Kevin Alexander: Removed unneccesary functions
+ */
+public interface IDatabaseCommunications {
+       
+        public List<Defect> getDefectList();   
+        public void addDefect( Defect defect );
+        public Defect getDefect( Integer defectId );
+        public void saveDefect( Defect defect );
+        
+        public void addUser(Person p);
+        public void deleteUser(Person p);
+        public Person getUser(int id);
+        
+        public Status getStatus(int id);
+        public void addStatus(Status status);
+        public void deleteStatus(Status status);
+        
+        public List<Status> getStatusList();      
+        public List<Person> getUsersList(); 
+        
+}
